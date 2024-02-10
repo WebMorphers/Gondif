f6486da20d08a9bf644afbdce4e5f0a50ee63ade
othsoh <145851559+WebMorphers@users.noreply.github.com>
2024-02-10 13:43:11 +0100

    Update layout.tsx

--- gondif/app/layout.tsx
+++ gondif/app/layout.tsx
@@ -1,6 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import type { Viewport } from 'next'
+ 
+export const viewport: Viewport = {
+  themeColor: 'black',
+  minimumScale: 1,
+  initialScale: 1,
+  width: 'device-width',
+  viewportFit: 'cover',
+}
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +21,6 @@ export const metadata: Metadata = {
   keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
   themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
   
-  viewport:
-    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
     { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
     { rel: "icon", url: "icons/icon-128x128.png" },
