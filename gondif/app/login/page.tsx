c1f76451345ba9213cbc4548bfee923fe5812be2
WebMorphers <145851559+WebMorphers@users.noreply.github.com>
2024-02-16 22:11:47 +0100

    e

--- gondif/app/login/page.tsx
+++ gondif/app/login/page.tsx
@@ -9,19 +9,19 @@ const Page = () => {
   const [value, setValue] = useState<string | undefined>(undefined); 
 
   return (
-    <div className='flex flex-col p-9'>
+    <div className='flex flex-col p-9 gap-5'>
       <h1 className='' >Enter your mobile number</h1>
-      <div className='flex flex-col'> 
+      <div className='flex flex-col gap-5'> 
         <PhoneInput 
           defaultCountry="MA"
           value={value}
           onChange={(newValue: string | undefined) => setValue(newValue)}  
         />
         <div><button
-  className="relative cursor-pointer opacity-90 hover:opacity-100 transition-opacity p-[2px] bg-black rounded-[16px] bg-gradient-to-t from-[#9FE870] to-[#9FE870] active:scale-95"
+  className="max-sm:w-full relative cursor-pointer opacity-90 hover:opacity-100 transition-opacity p-[2px] bg-black rounded-[16px] bg-gradient-to-t from-[#9FE870] to-[#9FE870] active:scale-95"
 >
   <span
-    className="w-full h-full flex items-center gap-2 px-8 py-3 bg-[#9FE870] text-[#f1d5fe] rounded-[14px] bg-gradient-to-t from-[#9FE870] to-[#9FE870]"
+    className="w-full h-full flex items-center gap-2 px-8 py-3 bg-[#9FE870] text-[#163300] rounded-[14px] bg-gradient-to-t from-[#9FE870] to-[#9FE870]"
   >
     Next</span>
 </button>
