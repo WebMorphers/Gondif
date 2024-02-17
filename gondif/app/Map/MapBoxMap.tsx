c9c5510fa258b10784360fd81f9eb2f74d1ae385
othsoh <145851559+WebMorphers@users.noreply.github.com>
2024-02-17 14:49:51 +0100

    Update MapBoxMap.tsx

--- gondif/app/Map/MapBoxMap.tsx
+++ gondif/app/Map/MapBoxMap.tsx
@@ -1,20 +1,21 @@
+import { Content } from 'next/font/google'
 import React from 'react'
 import { Map } from 'react-map-gl'
 
 export default function MapBoxMap() {
-  return (
+return (
     <div>    
-        <h1>Map</h1>
-        <Map
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOCKEN}
-        initialViewState={{
-            longitude: -122.4,
-            latitude: 37.8,
-            zoom: 14
-        }}
-        style={{width: 600, height: 400}}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-        />
+            <h1>Map</h1>
+            <Map
+            mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOCKEN}
+            initialViewState={{
+                    longitude: -122.4,
+                    latitude: 37.8,
+                    zoom: 14
+            }}
+            style={{width: 600, height: 1000}}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            />
     </div>
-  )
+)
 }
