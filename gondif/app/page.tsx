cb71e76f9576e51f525fb4f3f479a30b1dd94894
othsoh <145851559+WebMorphers@users.noreply.github.com>
2024-02-18 22:49:13 +0100

    t

--- gondif/app/page.tsx
+++ gondif/app/page.tsx
@@ -1,62 +1,97 @@
-'use client';
-
-import { signOut, useSession } from 'next-auth/react';
-import MapboxMap from '@/app/Map/MapBoxMap';
-import { useEffect, useState } from 'react';
-import { UserLocationContext } from '@/context/UserLocationContext';
-import DropdownMenu from '@/app/components/dropDown/dropdownMenu';
-import AutoCompleteAdress from './components/AutoCompleteAdress';
+"use client"
+import { Drawer } from 'vaul';
 
 export default function Home() {
-  const session = useSession();
-
-  useEffect(() => {
-    getUserLocation()
-  },[])
-  
-  const [UserLocation,setUserLocation] = useState<any>();
-
-  const getUserLocation=()=> { navigator.geolocation.getCurrentPosition(function(props){
-    console.log(props);
-    setUserLocation({
-      lat:props.coords.latitude,
-      lng:props.coords.longitude
-    })
-  })}
-
-
   return (
-    <div className="h-screen relative w-full z-40">
-      <UserLocationContext.Provider value={{UserLocation,setUserLocation}}>
-
-        <DropdownMenu />
-      <MapboxMap />
-      { UserLocation  ?
-       <div className='absolute z-50 h-[25%] w-full bg-white bottom-0 rounded-t-2xl flex flex-col items-center justify-center'>
-        <div className='mt-1 bg-[#C1C4C2] z-50 h-1 rounded-full w-[10%] text-center fix'></div>
-
-        
-        <div className='flex flex-col px-5 my-3 h-full w-full justify-between'>
-          <h1 className='font-bold text-[#163300] text-xl'>Your Position</h1>
-          <div className='mx-1 '>
-            <div className='flex justify-between pr-1'>
-            
-            <AutoCompleteAdress />
-
-
+    <Drawer.Root >
+            <Drawer.Trigger asChild>
+        <button>Open Drawer</button>
+      </Drawer.Trigger>
+      <Drawer.Portal>
+        <Drawer.Overlay className="fixed inset-0 bg-black/40" />
+          <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
+            <div className="p-4 bg-white rounded-t-[10px] flex-1">
+              <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mb-8" />
+              <div className="max-w-md mx-auto">
+                <Drawer.Title className="font-medium mb-4">
+                  Unstyled drawer for React.
+                </Drawer.Title>
+                <p className="text-zinc-600 mb-2">
+                  This component can be used as a replacement for a Dialog on
+                  mobile and tablet devices.
+                </p>
+                <p className="text-zinc-600 mb-8">
+                  It uses{" "}
+                  <a
+                    href="https://www.radix-ui.com/docs/primitives/components/dialog"
+                    className="underline"
+                    target="_blank"
+                  >
+                    Radix&apos;s Dialog primitive
+                  </a>{" "}
+                  under the hood and is inspired by{" "}
+                  <a
+                    href="https://twitter.com/devongovett/status/1674470185783402496"
+                    className="underline"
+                    target="_blank"
+                  >
+                    this tweet.
+                  </a>
+                </p>
+              </div>
             </div>
-            <div className='mx-1'>
-              <button>
-                Confirm Position
-              </button>
+            <div className="p-4 bg-zinc-100 border-t border-zinc-200 mt-auto">
+              <div className="flex gap-6 justify-end max-w-md mx-auto">
+                <a
+                  className="text-xs text-zinc-600 flex items-center gap-0.25"
+                  href="https://github.com/emilkowalski/vaul"
+                  target="_blank"
+                >
+                  GitHub
+                  <svg
+                    fill="none"
+                    height="16"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    width="16"
+                    aria-hidden="true"
+                    className="w-3 h-3 ml-1"
+                  >
+                    <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+                    <path d="M15 3h6v6"></path>
+                    <path d="M10 14L21 3"></path>
+                  </svg>
+                </a>
+                <a
+                  className="text-xs text-zinc-600 flex items-center gap-0.25"
+                  href="https://twitter.com/emilkowalski_"
+                  target="_blank"
+                >
+                  Twitter
+                  <svg
+                    fill="none"
+                    height="16"
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    viewBox="0 0 24 24"
+                    width="16"
+                    aria-hidden="true"
+                    className="w-3 h-3 ml-1"
+                  >
+                    <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+                    <path d="M15 3h6v6"></path>
+                    <path d="M10 14L21 3"></path>
+                  </svg>
+                </a>
+              </div>
             </div>
-          </div>
-        </div>
-
-      </div>
-
-        : null}
-      </UserLocationContext.Provider>
-    </div>
+          </Drawer.Content>
+      </Drawer.Portal>
+    </Drawer.Root>
   );
 }
