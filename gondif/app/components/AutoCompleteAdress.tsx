526b4d30123960b63c63c526f741e713d8e63d95
othsoh <145851559+WebMorphers@users.noreply.github.com>
2024-02-18 18:55:18 +0100

    Update AutoCompleteAdress.tsx

--- gondif/app/components/AutoCompleteAdress.tsx
+++ gondif/app/components/AutoCompleteAdress.tsx
@@ -4,6 +4,7 @@ function AutoCompleteAdress() {
   
 
 
+
   const [source,setSource] = useState<any>();
 
   const [AdressList,setAdressList] = useState<any>([]);
@@ -27,18 +28,16 @@ function AutoCompleteAdress() {
   }
 
   return (
-    <div>
-              
-        <div className='bg-black'>
-        {AdressList?.suggestions?.map((adress:any,index:number)=>(
-          <h2>{adress.suggestions.name}</h2>
-        ))
-      }
-        </div>
-              
-
+    <div >
         <label>Your Adress</label>
         <input type="text" className='bg-white p-1 border w-full rounded-md outline-none ' value={source} onChange={(e)=>{setSource(e.target.value); console.log("targeted")}}></input>
+        {AdressList?.suggestions ? 
+        <div className='bg-black h-full '>
+        {AdressList?.suggestions.map((item:any,index:number)=>(
+          <h2>{item[index].full_adress}</h2>
+        
+          ))}
+        </div>:null}
     </div>
   )
 }
