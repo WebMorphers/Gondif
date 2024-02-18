aa920bbe8ef0e67aefce2ca2ea2c24abf727004e
WebMorphers <145851559+WebMorphers@users.noreply.github.com>
2024-02-18 23:09:45 +0100

    j

--- gondif/app/page.tsx
+++ gondif/app/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 import { Drawer } from 'vaul';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(true);  
+
   return (
-    <Drawer.Root >
-            <Drawer.Trigger asChild>
-        <button>Open Drawer</button>
-      </Drawer.Trigger>
+    <Drawer.Root open={isOpen} onClose={() => setIsOpen(false)}>  
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
           <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-[10px] h-[96%] mt-24 fixed bottom-0 left-0 right-0">
