d67176fca50dd90422f7a899ad718121195d2edd
othsoh <145851559+WebMorphers@users.noreply.github.com>
2024-02-21 12:39:50 +0100

    Update MapBoxMap.tsx

--- gondif/app/Map/MapBoxMap.tsx
+++ gondif/app/Map/MapBoxMap.tsx
@@ -14,87 +14,73 @@ import { MapflytoWraper, useMapFlyToContext } from '@/context/Mapflytocontext'
 
 
 export default function MapBoxMap() {
-
-    const mapRef=useRef<any>();
-
+    const mapRef = useRef<any>();
+    const [markerCoordinates, setMarkerCoordinates] = useState<any>(null);
+    
     const { Coordinates, setCoordinates } = useCoordinatesContext();
-    const { userLocation,setUserLocation } = useUserLocationContext();
-    const { mapTriggerFlyTo,setMapTriggerFlyTo } = useMapFlyToContext()
-    const {currentLocation,setCurrentLocation} = useMapFlyToContext()
+    const { userLocation, setUserLocation } = useUserLocationContext();
+    const { mapTriggerFlyTo, setMapTriggerFlyTo } = useMapFlyToContext();
 
-    function flytocordinants(lng: any,lat: any){
+    function flytocordinants(lng: any, lat: any) {
         mapRef.current?.flyTo({
-            center: [lng,lat],
+            center: [lng, lat],
             zoom: 14,
-            duration:2500
-        })
+            duration: 2500
+        });
+        // Update marker coordinates
+        setMarkerCoordinates({ lng, lat });
     }
-    useEffect(()=> {
-        flytocordinants(userLocation?.lng,userLocation?.lat)
-     },[mapTriggerFlyTo])
 
-    useEffect(()=> {
-        if(Coordinates)
-        flytocordinants(Coordinates.lng,Coordinates.lat)
-    },[Coordinates])
+    useEffect(() => {
+        flytocordinants(userLocation?.lng, userLocation?.lat);
+    }, [mapTriggerFlyTo]);
 
-    useEffect(()=> {
-        console.log(currentLocation)
-    })
-return (
-    <MapflytoWraper>
-    <UserLocationWraper>
-<CoordinatesWraper>
-<div className='h-screen w-screen flex justify-center items-center'> 
-        {userLocation?
-            
-            <Map 
-        ref={mapRef}
-        mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOCKEN}
+    useEffect(() => {
+        if (Coordinates)
+            flytocordinants(Coordinates.lng, Coordinates.lat);
+    }, [Coordinates]);
 
-        initialViewState={{
-                longitude: userLocation?.lng, 
-                latitude: userLocation?.lat,
-                zoom: 14
-        }}
-        style={{width: '100%', height: '100%', background: 'linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0))'}}
-        mapStyle="mapbox://styles/mapbox/standard"        >
+    return (
+        <MapflytoWraper>
+            <UserLocationWraper>
+                <CoordinatesWraper>
+                    <div className='h-screen w-screen flex justify-center items-center'>
+                        {userLocation ?
+                            <Map
+                                ref={mapRef}
+                                mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOCKEN}
+                                initialViewState={{
+                                    longitude: userLocation?.lng,
+                                    latitude: userLocation?.lat,
+                                    zoom: 14
+                                }}
+                                style={{ width: '100%', height: '100%', background: 'linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0))' }}
+                                mapStyle="mapbox://styles/mapbox/standard">
 
-            
-            <NavigationControl position='top-left' />
+                                <NavigationControl position='top-left' />
+                                <AttributionControl customAttribution="Gondif" />
 
-            <AttributionControl customAttribution="Gondif" />
-
-            {Coordinates&&currentLocation==="search"?
-
-            <Marker 
-            longitude={Coordinates?.lng}
-            latitude={Coordinates?.lat}
-            anchor="bottom" 
-            >
-                <Image src={markerIcon} width={40} alt='marker' />
-            </Marker>
-            :
-            <Marker 
-            longitude={userLocation?.lng}
-            latitude={userLocation?.lat}
-            anchor="bottom" 
-            >
-                <Image src={markerIcon} width={40} alt='marker' />
-            </Marker>}   
-        </Map>
-        : 
-        <div className='py-12 flex justify-center items-center flex-col gap-3'>
-            <h1 className='text-center text-2xl font-semibold '>
-                Allow Position access
-            </h1>
-            <span className='font-extralight text-sm text-center'>You must allow access to continue.</span>
-            <Image src={LocalisationIcon} width={100} alt='position'/>
-        </div>}
-        
-    </div>
-    </CoordinatesWraper>
-    </UserLocationWraper>
-    </MapflytoWraper>
+                                {markerCoordinates ? // Check if marker coordinates are available
+                                    <Marker
+                                        longitude={markerCoordinates.lng}
+                                        latitude={markerCoordinates.lat}
+                                        anchor="bottom">
+                                        <Image src={markerIcon} width={40} alt='marker' />
+                                    </Marker>
+                                    : null
+                                }
+                            </Map>
+                            :
+                            <div className='py-12 flex justify-center items-center flex-col gap-3'>
+                                <h1 className='text-center text-2xl font-semibold '>
+                                    Allow Position access
+                                </h1>
+                                <span className='font-extralight text-sm text-center'>You must allow access to continue.</span>
+                                <Image src={LocalisationIcon} width={100} alt='position' />
+                            </div>}
+                    </div>
+                </CoordinatesWraper>
+            </UserLocationWraper>
+        </MapflytoWraper>
     )
 }
