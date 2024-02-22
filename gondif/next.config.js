3f4e3b3760183797be44eac030b1be0873f4df92
WebMorphers <145851559+WebMorphers@users.noreply.github.com>
2024-02-22 21:15:57 +0100

    f

--- gondif/next.config.js
+++ gondif/next.config.js
@@ -1,12 +1,20 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa') ;
+ const withPWA = require('next-pwa') ;
 
 const nextConfig  = {
     ...withPWA({
         dest: 'public',
         register: true,
         skipWaiting: true,
-    })
-}
+    }),
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'firebasestorage.googleapis.com',
+            pathname: '**',
+          },
+        ],
+      },
+}   
 
 module.exports = nextConfig;
