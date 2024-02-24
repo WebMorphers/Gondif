71a9041ddd0d287eece306da4a5c06aa43b014d9
WebMorphers <145851559+WebMorphers@users.noreply.github.com>
2024-02-24 18:41:36 +0100

    n

--- gondif/app/otpcode/page.tsx
+++ gondif/app/otpcode/page.tsx
@@ -2,24 +2,49 @@
 
 import { NumberWraper, useNumberContext, } from '@/context/NumberContext';
 import React from 'react';
+import { OtpInput } from 'reactjs-otp-input';
 
+ 
 const OtpCode = () => {
     const { phoneNumber, setPhoneNumber} = useNumberContext() || {};
     const {otp,setOtp} = useNumberContext() || {};
     const {confirmationResult,setconfirmationResult} = useNumberContext() || {};
     const {otpSent,setOtpSent} = useNumberContext() || {};
+    const lastFourDigits = phoneNumber ? phoneNumber.slice(-4).padStart(phoneNumber.length, '*') : '';
+    const [otptext, setOtptext] = React.useState('');
+     
+    const handleChange = (otp: any) => setOtptext(otp);
 
-    const maskPhoneNumber = (phoneNumber: string) => {
-        const maskedPart = phoneNumber?.slice(0, -4).replace(/\d/g, '*');
-        const lastFourDigits = phoneNumber?.slice(-4);
-        return maskedPart + lastFourDigits; 
-    };
-
-    return (
+      return (
         <NumberWraper>
-            <div>
-                <p>Enter the 4-digit code sent to you at {maskPhoneNumber(phoneNumber)}</p>
-            </div>
+            <div className='m-3  gap-7 flex flex-col items-start justify-center'>
+            <p>Enter the 4-digit code sent to you at <br /> {lastFourDigits}</p>
+
+                 <OtpInput value={otp} onChange={handleChange} 
+                  numInputs={6}
+                    separator={<span style={{ width: "5px" }}></span>}
+                    isInputNum={true}
+                    shouldAutoFocus={true}
+         inputStyle={{
+          border: "1px solid transparent",
+          borderRadius: "8px",
+          width: "45px",
+          backgroundColor:"#EEEEEE",
+          height: "45px",
+          fontSize: "15px",
+          color: "#000",
+          fontWeight: "400",
+          caretColor: "blue"
+        }}
+        focusStyle={{
+          border: "1px solid #2D2E2E",
+          outline: "none"
+        }} />
+
+<p className='text-[#2D2E2E] bg-[#EEEEEE] rounded-full p-2 text-sm'>I haven’t received a code</p>
+
+
+                        </div>
         </NumberWraper>
     );
 };
